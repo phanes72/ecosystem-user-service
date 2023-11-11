43cc4f7051d8b208546e10ac9396be4058b46801
Patrick Hanes <phanes72@gmail.com>
2023-11-11 06:29:36 -0700

    Fully integrated Angular app in a Spring Boot App

--- src/main/java/com/careydevelopment/ecosystem/user/ecosystem_user_service/HelloWorldController.java
+++ src/main/java/com/careydevelopment/ecosystem/user/ecosystem_user_service/HelloWorldController.java
@@ -1,10 +1,12 @@
 package com.careydevelopment.ecosystem.user.ecosystem_user_service;
 
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class HelloWorldController {
 
     @GetMapping("/helloworld")
